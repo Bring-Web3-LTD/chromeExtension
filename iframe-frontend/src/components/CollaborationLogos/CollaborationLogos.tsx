2f91fbc84eaf40b7e97f033c2da557074becbcc5
henyaGreenberg <henyag@bring.network>
2026-01-26 13:44:47 +0200

    feat: add fallback initials for retailer logos and implement getInitials utility

--- iframe-frontend/src/components/CollaborationLogos/CollaborationLogos.tsx
+++ iframe-frontend/src/components/CollaborationLogos/CollaborationLogos.tsx
@@ -2,20 +2,35 @@ import styles from './styles.module.css'
 import PlatformLogo from '../PlatformLogo/PlatformLogo';
 import { useRouteLoaderData } from 'react-router-dom';
 import toCapital from '../../utils/toCapital';
+import { getInitials } from '../../utils/getInitials';
+import { useState } from 'react';
 
 const CollaborationLogos = () => {
-    const { iconUrl, name, platformName } = useRouteLoaderData('root') as LoaderData
+    const { iconUrl, name, platformName } = useRouteLoaderData('root') as LoaderData    
+    const [fallbackImg, setFallbackImg] = useState<string | null>(
+        !iconUrl || iconUrl.trim() === '' ? getInitials(name) : null
+    )
+    // const [fallbackImg, setFallbackImg] = useState<string | null>(getInitials(name))     
 
     return (
         <div id="collaboration-logos-container" className={styles.logos_container} >
             <div id="retailer-logo-container" className={styles.logo_container} >
                 <div id="retailer-logo-wrapper" className={styles.logo_wrapper} >
-                    <img
-                        id="retailer-logo"
-                        src={iconUrl}
-                        className={styles.logo}
-                        alt={`${name}-website-icon`}
-                    />
+                    {fallbackImg ? (
+                        <div 
+                            id="retailer-logo-initials"
+                            className={styles.logo_initials}
+                        >
+                            {fallbackImg}
+                        </div>
+                    ) : (
+                        <img
+                            id="retailer-logo"
+                            src={iconUrl}
+                            className={styles.logo}                            
+                            onError={() => setFallbackImg(getInitials(name))}
+                        />
+                    )}
                 </div>
                 <div id="retailer-logo-text" className={styles.logo_text}>
                     {name}

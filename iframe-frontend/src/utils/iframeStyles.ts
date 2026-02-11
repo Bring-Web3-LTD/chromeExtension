d952ebca8c9f290280c5b3baae72cce30a3c9651
henyaGreenberg <henyag@bring.network>
2026-02-11 08:19:34 +0200

    fix: update offerbar position

--- iframe-frontend/src/utils/iframeStyles.ts
+++ iframe-frontend/src/utils/iframeStyles.ts
@@ -38,7 +38,7 @@ export const offerbarStyle: Styles = {
             height: `482px`,
             borderRadius: '100px 0 0 100px',
             display: 'block',
-            top: '140px',
+            top: '190px',
             right: '0px'
         }
     }
